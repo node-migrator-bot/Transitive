a960efbe5886b0a42ecfe102f5a4c87793d3eebf
Aaron Blohowiak <aaron.blohowiak@gmail.com>
2011-05-20 17:43:06 -0700

    automatic generation of templates.js works

--- test/test_boot.js
+++ test/test_boot.js
@@ -1,9 +1,22 @@
-var x = new (require("transitive"))();
+var x = new (require("transitive"))(),
+    fs = require("fs");
+
+var options = {};
+var generated = process.cwd()+"/test_temp/"+require("uuid-pure").newId();
+fs.mkdirSync(generated, "777");
+
+exports.setUp = function (test, assert) {
+  process.chdir("./test/views");
+  options = x.loadOptions({});
+
+  options.directories.generated = generated;
+  options.mergeDefault = false;
+  x.options = options;
+  test.finish();
+};
 
 exports["test boot can work okay"] = function(test, assert){
-  x.boot(this);
-  assert.ok(x.options);
+  x.boot(this, options);
   assert.ok(x.options.root);
-  
   test.finish();
 };
